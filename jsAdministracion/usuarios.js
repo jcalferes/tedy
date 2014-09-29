31546bae11cb8ae3aa6e84ff14aced32621c17d6
torres <torres@torres-Lenovo-G460>
2014-09-29 09:34:02 -0500

    Area administrativa

---
+++ jsAdministracion/usuarios.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+//    alert("hola mundo");
+    $("#usuarios").click(function () {
+        $("#contenido").load("administracion_dameUsuarios.php");
+    });
+
+
+});
